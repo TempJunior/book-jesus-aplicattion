1ddec45641e971796f43fa29cb1bf990042434dc
TempJunior <junior_sadaanzinhoa@hotmail.com>
2025-06-27 15:52:33 -0300

    Implementando dados de usuario, jwtToken e destroy userData da store e persistindo

--- src/stores/axiosStore.ts
+++ src/stores/axiosStore.ts
@@ -1,6 +1,14 @@
 import { defineStore } from 'pinia'
 import axios, { type AxiosInstance } from 'axios'
 import { ref } from 'vue'
+import type IUserData from '@/interfaces/IUserData.ts'
+
+const options = {
+  persist: {
+    key: 'localStore',
+    storage: localStorage,
+  },
+}
 
 export const useAxiosStore = defineStore('axiosStore', () => {
   const axiosInstance = axios.create({
@@ -8,7 +16,35 @@ export const useAxiosStore = defineStore('axiosStore', () => {
     headers: { 'Content-Type': 'application/json' },
   })
 
+  const userData = ref<IUserData | null>(null)
+
+  const tokenJWT = ref<string | null>(null)
+
+
+  function setTokenJWT(token: string | null): void {
+    tokenJWT.value = token
+
+    if (token) {
+      localStorage.setItem('tokenJWT', token)
+    }else {
+      localStorage.removeItem('tokenJWT')
+    }
+  }
+
+  function setUserData (data: IUserData) {
+    userData.value = data
+  }
+
+  function clearUserData () {
+    userData.value = null
+    tokenJWT.value = null
+    localStorage.removeItem('tokenJWT')
+  }
+
   const http = ref<AxiosInstance>(axiosInstance)
 
-  return { http }
-})
+  return { http, userData, setUserData, clearUserData, tokenJWT, setTokenJWT }
+},
+  options,
+)
+
