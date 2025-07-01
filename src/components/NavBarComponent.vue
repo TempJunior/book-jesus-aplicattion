26174148c0871a68690addc59405c7084454ec25
TempJunior <junior_sadaanzinhoa@hotmail.com>
2025-07-01 17:39:50 -0300

    Tela de update user

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -76,6 +76,7 @@ console.log(store.userData)
         >
           <li>
             <a
+              @click="router.push({name: 'home'})"
               href="#"
               class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
               aria-current="page"
---
+++ src/views/UserAccountView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import NavBarComponent from '@/components/NavBarComponent.vue'
+import UpdatingForms from '@/components/UpdatingForms.vue'
+</script>
+
+<template>
+<div class="bg-gray-50 dark:bg-gray-800 h-full">
+  <NavBarComponent/>
+
+  <section class="flex flex-col p-12 items-start gap-12 h-screen">
+    <div>
+      <h1 class="text-7xl">Junior</h1>
+    </div>
+    <UpdatingForms/>
+  </section>
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
